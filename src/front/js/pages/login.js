deffed2b7a4066e563a1bf3df968b876eb4e3b89
MaiCodes-exe <maise.b.costa@gmail.com>
2022-04-27 13:43:10 +0000

    added hooks for session

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -1,5 +1,28 @@
-import React, { useContext, useState } from "react";
+import React, { useState, useContext, useEffect } from "react";
+import { Context } from "../store/appContext";
+import { Link, useHistory } from "react-router-dom";
+
 export const LogIn = () => {
+  const { store, actions } = useContext(Context);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  let history = useHistory();
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      actions.setToken(localStorage.getItem("token"));
+      history.push("/");
+    }
+  }, []);
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
   return (
     <div className="text-center mt-5">
       <h1> Login</h1>
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,49 +1,74 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-
-			getMessage: () => {
-				// fetching data from the backend
-				fetch(process.env.BACKEND_URL + "/api/hello")
-					.then(resp => resp.json())
-					.then(data => setStore({ message: data.message }))
-					.catch(error => console.log("Error loading message from backend", error));
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
+  return {
+    store: {
+      message: null,
+      api: "https://3000-ertip4geek-swauthenticat-5z0owhl85dg.ws-eu34xl.gitpod.io",
+    },
+    actions: {
+      login: (email, password) => {
+        const store = getStore();
+        fetch(`${store.api}/api/login`, {
+          method: "POST",
+          body: JSON.stringify({
+            email: email,
+            password: password,
+          }),
+          headers: {
+            "Content-type": "application/json",
+          },
+        })
+          .then((resp) => {
+            if (resp.ok) {
+              return resp.json();
+            }
+          })
+          .then((data) => {
+            localStorage.setItem("token", data.token);
+            setStore({ isAuthenticate: true });
+          })
+          .catch((error) => console.error("[ERROR IN LOGIN]", error));
+      },
+      signup: (email, password, roles) => {
+        // const store = getStore();
+        fetch(process.env.BACKEND_URL + `/api/signup`, {
+          method: "POST",
+          body: JSON.stringify({
+            email: email,
+            password: password,
+            roles: roles,
+          }),
+          headers: {
+            "Content-type": "application/json",
+          },
+        })
+          .then((resp) => {
+            console.log(resp);
+            if (resp.ok) {
+              console.log(resp);
+              return resp.json();
+            }
+          })
+          .then((data) => {
+            console.log(data);
+            localStorage.setItem("token", data.token);
+            setStore({ isAuthenticate: true });
+          })
+          .catch((error) => console.error("[ERROR IN LOGIN]", error));
+      },
+      setToken: (token) => {
+        setStore({ token: token });
+      },
+      syncTokenFromSessionStore: () => {
+        const token = localStorage.getItem("token");
+        if (token && token != "" && token != undefined)
+          setStore({ token: token });
+      },
+      logout: () => {
+        localStorage.removeItem("token");
+        console.log("Logout");
+        setStore({ token: null });
+      },
+    },
+  };
 };
-
 export default getState;
