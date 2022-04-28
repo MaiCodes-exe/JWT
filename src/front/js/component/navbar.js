331c72232f94a08b99ecc3152d6773a16da81194
MaiCodes-exe <maise.b.costa@gmail.com>
2022-04-28 10:55:31 +0000

    added buttons and links

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -2,18 +2,29 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <div className="container">
+        <Link to="/">
+          <span className="navbar-brand mb-0 h1">React Boilerplate</span>
+        </Link>
+        <div className="ml-auto">
+          <div>
+            <div className="d-flex">
+              <button type="button" className="btn" id="button1">
+                <Link to="/login" id="log">
+                  Login
+                </Link>
+              </button>
+              <button type="button" className="btn" id="sign">
+                <Link to="/signup" id="ss">
+                  Sign Up
+                </Link>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
