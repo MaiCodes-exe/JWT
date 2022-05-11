c548ef3dfb1ca8ab68cc2969b15b317587863448
MaiCodes-exe <maise.b.costa@gmail.com>
2022-05-11 19:21:26 +0000

    added content to private are

--- src/front/js/pages/private.js
+++ src/front/js/pages/private.js
@@ -1,5 +1,7 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
+import { Link } from "react-router-dom";
+
 import "../../styles/home.css";
 
 export const Private = () => {
@@ -7,7 +9,10 @@ export const Private = () => {
 
   return (
     <div className="text-center mt-5">
-      <h1> Welcome</h1>
+      <h1> Welcome. You are now logged in</h1>
+      <button type="button" className="btn" id="los">
+        <Link to="/">LogOut</Link>
+      </button>
     </div>
   );
 };
